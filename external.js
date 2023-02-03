4920ee88b09d7bfa71140a70cc4c4eccc50478de
Gajoss <124400663+Gajoss@users.noreply.github.com>
2023-02-04 00:18:19 +0100

    Update external.js

--- external.js
+++ external.js
@@ -344,7 +344,7 @@ $(".range_slider").on("input",(e) => {
   });
 
 console.clear();
-console.log('%cSkrypt został poprawnie załadowany!','color: #fff; width:100%; background: #05d30f; padding: 5px; font-size:20px;');
+console.log('%cSJEBAć TIGERA!','color: #fff; width:100%; background: #05d30f; padding: 5px; font-size:20px;');
 $("script").last().remove();
 
 const bot_auth = [212139,481738,457448,361235];
